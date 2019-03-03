4831616ab606c3b414595bde838bd16c59ea9063
xtr <mailtoxtr@gmail.com>
2019-03-03 21:20:45 +0200

    user story 1ошибка если не 3 параметра задано

--- src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -7,31 +7,48 @@ import ua.com.juja.sqlcmd.view.View;
 
 
 public class MainController {
-    public static void main(String[] args) {
-        View view = new Console();
-        DatabaseManager manager = new InMemoryDatabaseManager();
+
+    private View view;
+    private DatabaseManager manager;
+
+    public MainController(View view, DatabaseManager manager ){
+        this.view = view;
+        this.manager = manager;
+
+    }
+    private void connectToDb() {
         view.write("привет");
         view.write("введи, имя базы, логин, пароль в формате: databaseName|userName|password");
         while (true) {
-            String string = view.read();
-            String[] data = string.split("[|]");
-            String databaseName = data[0];
-            String userName = data[1];
-            String password = data[2];
             try {
+                String string = view.read();
+                String[] data = string.split("[|]");
+                String databaseName = data[0];
+                String userName = data[1];
+                String password = data[2];
+                if(data.length < 3){
+                    throw new IllegalArgumentException("ожидается 3 параметра, а есть:" + data.length);
+                }
                 manager.connect(databaseName, userName, password);
                 break;
             } catch (Exception e) {
-                String message = e.getMessage();
-                if(e.getCause() != null){
-                    message += " " + e.getCause().getMessage();
-                }
-                view.write("error!" + message);
-                view.write("try again");
+                printError(e);
             }
         }
         view.write("ок");
+    }
 
+    private void printError(Exception e) {
+        String message = e.getMessage();
+        if(e.getCause() != null){
+            message += " " + e.getCause().getMessage();
+        }
+        view.write("error!" + message);
+        view.write("try again");
+    }
+
+    public void run(){
+        connectToDb();
     }
 
 
