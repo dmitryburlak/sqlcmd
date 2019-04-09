489e21ae958435fe649bea1523c6de1ece1d13f4
xtr <mailtoxtr@gmail.com>
2019-04-09 15:20:15 +0300

    тестим clear

---
+++ src/test/java/ua/com/juja/sqlcmd/controller/command/ClearTest.java
@@ -0,0 +1,67 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import ua.com.juja.sqlcmd.model.DataSet;
+import ua.com.juja.sqlcmd.model.DatabaseManager;
+import ua.com.juja.sqlcmd.view.View;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+
+public class ClearTest {
+
+    private DatabaseManager manager;
+    private View view;
+    private Command command;
+
+    @Before
+    public void setup(){
+        manager = mock(DatabaseManager.class);
+        view = mock(View.class);
+        command = new Clear(manager, view);
+    }
+    @Test
+    public void testClearTable(){
+
+        //given
+
+        //when
+        command.process("clear|newlist");
+        //then
+        verify(manager).clear("newlist");
+        verify(view).write("таблица newlist очищена");
+
+    }
+
+
+
+    @Test
+    public void testCanProcessClearWithParametersString(){
+        //given
+
+
+        //when
+        boolean canProcess = command.canProcess("clear|");
+
+        //then
+        assertTrue(canProcess);
+    }
+
+    @Test
+    public void testNotCanProcessClearWithoutParametersString(){
+        //given
+
+
+        //when
+        boolean canProcess = command.canProcess("qwe|");
+
+        //then
+        assertFalse(canProcess);
+    }
+
+
+}
