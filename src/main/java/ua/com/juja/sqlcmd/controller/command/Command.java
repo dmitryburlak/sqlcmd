6ce4f81e68a4d1c0a40113e3fe4eff76fe69446c
xtr <mailtoxtr@gmail.com>
2019-05-29 22:14:27 +0300

    TODO доделать

--- src/main/java/ua/com/juja/sqlcmd/controller/command/Command.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/command/Command.java
@@ -5,4 +5,8 @@ public interface Command {
     boolean canProcess (String command);
 
     void process (String command);
+
+    //TODO выделить новым методом формат и описание команд
+    //String format();
+    //String description();
 }
