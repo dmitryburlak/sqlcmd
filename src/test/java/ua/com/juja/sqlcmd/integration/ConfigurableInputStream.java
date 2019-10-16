972cbef3eed1cf7275045a87edb96c3ba48058bf
xtr <mailtoxtr@gmail.com>
2019-10-16 22:36:10 +0300

    рефакторинг  тест configurableinputstream

--- src/test/java/ua/com/juja/sqlcmd/integration/ConfigurableInputStream.java
+++ src/test/java/ua/com/juja/sqlcmd/integration/ConfigurableInputStream.java
@@ -8,30 +8,24 @@ public class ConfigurableInputStream extends InputStream {
     private String line;
     private boolean endLine = false;
 
-
-
     @Override
     public int read() throws IOException {
-
-        if (line.length() == 0){
+        if (line.length() == 0) {
             return -1;
         }
-        if (endLine){
+        if (endLine) {
             endLine = false;
             return -1;
         }
         char ch = line.charAt(0);
         line = line.substring(1);
-
-        if (ch == '\n'){
+        if (ch == '\n') {
             endLine = true;
         }
-
-        return (int)ch;
+        return (int) ch;
     }
 
     public void add(String line) {
-
         if (this.line == null){
             this.line = line;
         } else {
