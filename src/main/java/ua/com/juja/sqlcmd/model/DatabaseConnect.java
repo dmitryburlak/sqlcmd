909fe875df6f7a857f4960faaca802dd8e9ec51a
xtr <mailtoxtr@gmail.com>
2019-10-23 22:23:09 +0300

    заекстрактил метод setupDriver

--- src/main/java/ua/com/juja/sqlcmd/model/DatabaseConnect.java
+++ src/main/java/ua/com/juja/sqlcmd/model/DatabaseConnect.java
@@ -1,36 +1,39 @@
 package ua.com.juja.sqlcmd.model;
 
-import ua.com.juja.sqlcmd.view.MessageList;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import static ua.com.juja.sqlcmd.view.MessageList.*;
 
 public class DatabaseConnect {
 
     private static Connection connection;
 
     public static Connection connect(String database, String userName, String password) {
-
-        ConnectProperties connectProperties = new ConnectProperties().readProperties();
-        String driver = connectProperties.getDriver();
-        String host = connectProperties.getHost();
-
-        try {
-            Class.forName(driver);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(MessageList.DRIVER_NOT_FOUND.getMessage() + e);
-        }
+        String host = setupDriver();
         try {
             connection = DriverManager.getConnection(host + database, userName,
                     password);
         } catch (SQLException e) {
             connection = null;
-            throw new RuntimeException(String.format(MessageList.NOT_CONNECTION.getMessage(),
+            throw new RuntimeException(String.format(NOT_CONNECTION.getMessage(),
                     database, userName), e);
         }
         return connection;
     }
+
+    private static String setupDriver() {
+        ConnectProperties connectProperties = new ConnectProperties().readProperties();
+        String driver = connectProperties.getDriver();
+        String host = connectProperties.getHost();
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(DRIVER_NOT_FOUND.getMessage() + e);
+        }
+        return host;
+    }
+
     public static Connection getConnection() {
         return connection;
     }
