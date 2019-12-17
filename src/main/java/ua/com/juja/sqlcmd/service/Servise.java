ff6cecd5cba61be4a1fb1ea08c13a075fb39c76f
xtr <mailtoxtr@gmail.com>
2019-12-18 00:07:01 +0200

    убрал manager

--- src/main/java/ua/com/juja/sqlcmd/service/Servise.java
+++ src/main/java/ua/com/juja/sqlcmd/service/Servise.java
@@ -7,24 +7,25 @@ import java.util.List;
 public interface Servise {
     List<String> commandsList();
 
-    DatabaseManager connect(String database, String userName, String password) throws ServiseException;
+    void connect(String database, String userName, String password) throws ServiseException;
 
-    List<List<String>> find(DatabaseManager manager, String tableName) throws ServiseException;
+    List<List<String>> find(String tableName) throws ServiseException;
 
-    String tables(DatabaseManager manager) throws ServiseException;
+    String tables() throws ServiseException;
 
-    void insert(DatabaseManager manager, String tableName, String column, String value, String columnsecond,
+    void insert(String tableName, String column, String value, String columnsecond,
                 String valuesecond) throws ServiseException;
 
-    void create(DatabaseManager manager, String tableName, String columnPk, String columnone,
+    void create(String tableName, String columnPk, String columnone,
                 String columntwo) throws ServiseException;
 
-    void delete(DatabaseManager manager, String tableName, String column, String value) throws ServiseException;
+    void delete(String tableName, String column, String value) throws ServiseException;
 
-    void drop(DatabaseManager manager, String tableName) throws ServiseException;
+    void drop(String tableName) throws ServiseException;
 
-    void update(DatabaseManager manager, String tableName, int id, String column, String value) throws ServiseException;
+    void update(String tableName, int id, String column, String value) throws ServiseException;
 
-    void clear(DatabaseManager manager, String tableName) throws ServiseException;
+    void clear(String tableName) throws ServiseException;
 
+    DatabaseManager getManager();
 }
